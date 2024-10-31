03c1a78eac7449d9b8191b1940f00a90fff109c3
Yashu <ayushpal1944@gmail.com>
2024-10-31 18:05:00 +0530

    first commit

---
+++ Learn_Redux/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <div className='bg-red-100 mt-4 py-8 mx-40'>
+      <h1 className='text-2xl font-bold text-center text-red-900'>Let's Start Redux Toolkit</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
